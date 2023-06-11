c039b867c2066b682eb902f5a8f77717aec09976
hemant0933 <116268777+hemant0933@users.noreply.github.com>
2023-06-11 19:49:58 +0530

    add some styling

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -2,42 +2,65 @@ import React, { useContext, useState } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 const AddTransaction = () => {
-  const {addTransaction} = useContext(GlobalContext);
+  const { addTransaction } = useContext(GlobalContext);
 
-    const [text,setText] = useState("");
-    const [amount,setAmount] = useState(0);
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
 
-    const onSubmit = (e) =>{
-      e.preventDefault();
+  const onSubmit = (e) => {
+    e.preventDefault();
 
-      const newTransaction = {
-        id: Math.floor(Math.random() * 1000000),
-        text,
-        amount:+amount
-      }
-      addTransaction(newTransaction);
-    }
-
+    const newTransaction = {
+      id: Math.floor(Math.random() * 1000000),
+      text,
+      amount: +amount,
+    };
+    addTransaction(newTransaction);
+  };
 
   return (
-    <div  className="trans">
+    <div className="trans">
       <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
-          <label htmlFor="text">Text</label><br/>
-          <input type="text" id="text" value={text} onChange={(e)=>setText(e.target.value)} placeholder="Enter text..." />
+          <label htmlFor="text">Text</label>
+          <br />
+          <div class="input-wrapper">
+            <input
+              type="text"
+              id="text"
+              className="input"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              placeholder="Enter text..."
+            />
+          </div>
         </div>
         <div className="form-control">
           <label htmlFor="amount">
             Amount
             <br />
             (Negative - Expense, Positive - Income)
-          </label><br/>
-          <input type="number" id="amount" value={amount} onChange={(e)=>setAmount(e.target.value)} placeholder="Enter amount..."/>
+          </label>
+          <br />
+          <div class="input-wrapper">
+            <input
+              type="number"
+              id="amount"
+              className="input"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              placeholder="Enter amount..."
+            />
+          </div>
         </div>
-        <button className="transaction" type="submit">Add Transaction</button>
+        {/* <button className="transaction" type="submit">
+          Add Transaction
+        </button> */}
+        <button class="button"  type="submit">
+        Add Transaction
+         </button>
       </form>
-      
     </div>
   );
 };
